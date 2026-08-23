8f38fb9a7abd416da7d4d3170d4b75380e7aff59
Arti277 <artigidhad72@gmail.com>
2026-08-23 13:29:57 +0530

    update backend setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
+dotenv.config({
+  path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
 import express from "express"
 const app=express()
 
 
 (async () => {
     try{
-      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
       app.on("error",(error)=> {
         console.log("ERRR:",error);
         throw error
@@ -22,3 +39,4 @@ const app=express()
         throw err
     }
 })()
+    */
