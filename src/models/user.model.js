aa67ddefc4d5d09edcf18705a6e71318652fde57
Arti277 <artigidhad72@gmail.com>
2026-09-09 20:30:51 +0530

    configuration setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -80,7 +80,7 @@ userSchema.methods.isPasswordCorrect=async function(password)
 }
 
 userSchema.methods.generateAccessToken=function(){
-    jwt.sign({
+   return jwt.sign({
        _id: this._id ,
        email:this.email,
        username:this.username,
@@ -94,6 +94,16 @@ userSchema.methods.generateAccessToken=function(){
 )
 
 }//Access token
-userSchema.methods.generateRefreshToken=function(){}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+       _id: this._id ,
+      
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:  process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const user=mongoose.model("User",userSchema)
